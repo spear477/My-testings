c727b322c52fac5b8906cc9c6f0d940555518abe
spear477 <aungkokomin661@gmail.com>
2022-02-07 10:42:54 +0630

    first

---
+++ newapi.js
@@ -0,0 +1,266 @@
+let data ={
+    "status": "ok",
+    "totalResults": 13442,
+    "articles": [
+    {
+    "source": {
+    "id": "rt",
+    "name": "RT"
+    },
+    "author": "RT",
+    "title": "GoFundMe walks back ‘Freedom Convoy’ plans",
+    "description": "GoFundMe has agreed to refund Freedom Convoy donations after the threat of fraud investigations by Florida Governor Ron DeSantis and others Read Full Article at RT.com",
+    "url": "https://www.rt.com/news/548468-gofundme-walks-back-freedom-convoy-plans/",
+    "urlToImage": "https://cdni.rt.com/files/2022.02/article/61ffba9185f5402be81de978.jpg",
+    "publishedAt": "2022-02-06T12:11:10Z",
+    "content": "Crowdfunding platform GoFundMe has reversed its decision to redistribute nearly $8 million (Can$10 million) in donations made to Canada’s ‘Freedom Convoy’ protest following backlash on social media a… [+2363 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Golem.de"
+    },
+    "author": "Golem.de",
+    "title": "Automatisiertes Fahren: Unfall mit Tesla Full Self-Driving Beta auf Video",
+    "description": "Tesla muss seine Full-self-Driving-Funktion mit deutlich mehr Fähigkeiten ausstatten, um Unfälle wie den auf einem Video gezeigten zu verhindern. (Tesla, Technologie)",
+    "url": "https://www.golem.de/sonstiges/zustimmung/auswahl.html?from=https%3A%2F%2Fwww.golem.de%2Fnews%2Fautomatisiertes-fahren-unfall-mit-tesla-full-self-driving-beta-auf-video-2202-162963.html&referer=https%3A%2F%2Ft.co%2Ff080a65d8a",
+    "urlToImage": null,
+    "publishedAt": "2022-02-06T12:03:03Z",
+    "content": "Besuchen Sie Golem.de wie gewohnt mit Werbung und Tracking, indem Sie der Nutzung aller Cookies zustimmen.\r\n Details zum Tracking finden Sie im Privacy Center.\r\nSkript wurde nicht geladen. Informatio… [+575 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Golem.de"
+    },
+    "author": "Andreas Donath",
+    "title": "Automatisiertes Fahren: Unfall mit Tesla Full Self-Driving Beta auf Video",
+    "description": "Tesla muss seine Full-self-Driving-Funktion mit deutlich mehr Fähigkeiten ausstatten, um Unfälle wie den auf einem Video gezeigten zu verhindern. (Tesla, Technologie)",
+    "url": "https://www.golem.de/sonstiges/zustimmung/auswahl.html?from=https%3A%2F%2Fwww.golem.de%2Fnews%2Fautomatisiertes-fahren-unfall-mit-tesla-full-self-driving-beta-auf-video-2202-162963-rss.html&referer=https%3A%2F%2Ft.co%2F179edd4997",
+    "urlToImage": null,
+    "publishedAt": "2022-02-06T12:03:03Z",
+    "content": "Besuchen Sie Golem.de wie gewohnt mit Werbung und Tracking, indem Sie der Nutzung aller Cookies zustimmen.\r\n Details zum Tracking finden Sie im Privacy Center.\r\nSkript wurde nicht geladen. Informatio… [+575 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "DailyFX"
+    },
+    "author": "Margaret Yang, CFA",
+    "title": "Twitter and Pfizer Earnings in Focus After FAANG Deliver Divergent Results",
+    "description": "Wall Street equities had a rollercoaster week as FAANG earnings boosted market volatility. An uneven picture underscored challenges and opportunities ahead for big tech names.",
+    "url": "https://www.dailyfx.com/forex/fundamental/forecast/weekly/title/2022/02/06/Twitter-and-Pfizer-Earnings-in-Focus-After-FAANG-Deliver-Divergent-Results.html",
+    "urlToImage": "https://a.c-dn.net/b/1DXSi0/headline_NASDAQ_3.JPG",
+    "publishedAt": "2022-02-06T12:00:00Z",
+    "content": "S&amp;P 500 FORECAST: Bullish\r\n<ul><li>The S&amp;P 500 index may shrug off disappointing Facebook results after Amazon topped market expectations</li><li>So far in the earnings season, over 80% of th… [+3789 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Seeking Alpha"
+    },
+    "author": "BeanKounter Capital",
+    "title": "TQQQ: Patience Is A Virtue",
+    "description": "ProShares UltraPro QQQ ETF's (TQQQ) largest holdings had wild swings last week. See why I think investors with TQQQ on their watchlist should be patient here.",
+    "url": "https://seekingalpha.com/article/4484656-tqqq-proshares-etf-patience-virtue",
+    "urlToImage": "https://static.seekingalpha.com/cdn/s3/uploads/getty_images/857860874/image_857860874.jpg?io=getty-c-w750",
+    "publishedAt": "2022-02-06T12:00:00Z",
+    "content": "ronstik/iStock via Getty Images\r\nI wrote my previous article on TQQQ (TQQQ) when it was close to all-time highs. I mentioned that I'm planning to buy a small piece of TQQQ in 2022 and was waiting for… [+3788 chars]"
+    },
+    {
+    "source": {
+    "id": "fox-news",
+    "name": "Fox News"
+    },
+    "author": "Adam Shaw",
+    "title": "GoFundMe, now nixing Canadian trucker fundraiser, promoted Capitol Hill Occupied Protest appeal",
+    "description": "Crowdfunding platform GoFundMe, which is now caught up in controversy over its nixing of a fundraiser for the Canadian trucker protests, once promoted an appeal for the 2020 Seattle Capitol Hill Occupied Protest (CHOP.)",
+    "url": "https://www.foxnews.com/politics/gofundme-canadian-trucker-fundraiser-offered-support-capitol-hill-occupied-protest",
+    "urlToImage": "https://static.foxnews.com/foxnews.com/content/uploads/2022/02/2022-01-30T194322Z_2_LYNXMPEI0T08C_RTROPTP_4_HEALTH-CORONAVIRUS-CANADA-TRUCKING.jpg",
+    "publishedAt": "2022-02-06T11:30:13Z",
+    "content": "Crowdfunding platform GoFundMe, which is now caught up in controversy over its nixing of a fundraiser for the Canadian trucker protests, once promoted an appeal for the 2020 Seattle Capitol Hill Occu… [+3210 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Macitynet.it"
+    },
+    "author": "Mauro Notarianni",
+    "title": "eStream è la roulotte smart ad alimentazione elettrica",
+    "description": "Per ora è solo un concept ma eStream è una roulotte da sogno, con unità elettrica e batterie di supporto al veicolo trainante, pannelli solari e frenata rigenerativa.\n- su macitynet.it eStream è la roulotte smart ad alimentazione elettrica",
+    "url": "https://www.macitynet.it/estream-e-la-roulotte-smart-ad-alimentazione-elettrica/",
+    "urlToImage": "https://www.macitynet.it/wp-content/uploads/2022/02/eStream.jpg",
+    "publishedAt": "2022-02-06T11:29:55Z",
+    "content": "Airstream è un marchio americano di roulotte facilmente riconoscibili per la forma distintiva della loro carrozzeria in alluminio arrotondato e lucido.\r\nLe roulotte e i camper Airstream sono prodotti… [+2367 chars]"
+    },
+    {
+    "source": {
+    "id": "business-insider",
+    "name": "Business Insider"
+    },
+    "author": "ztayeb@businessinsider.com (Zahra Tayeb)",
+    "title": "Senator Ted Cruz bought up to $50,000 in bitcoin, according to a new financial disclosure",
+    "description": "In a disclosure to the senate filed Friday, Sen. Ted Cruz revealed he had bought up to $50,000 worth of the digital coin last month.",
+    "url": "https://www.businessinsider.com/senator-ted-cruz-bitcoin-crypto-financial-disclosure-2022-2",
+    "urlToImage": "https://i.insider.com/609ac209ba78eb001906eaf1?width=1200&format=jpeg",
+    "publishedAt": "2022-02-06T11:26:19Z",
+    "content": "Senator Ted Cruz bought up to $50,000 worth of bitcoin last month, according to a new financial disclosure filed with the US Senate. \r\nThe disclosure, first reported by Cointelegraph, shows that Cruz… [+1273 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Cool3c.com"
+    },
+    "author": "Mash Yang",
+    "title": "福特 Ford+ 轉型計畫將在 10 年內額外投資 200 億美元 加速電動車普及化",
+    "description": "目前福特在Ford+轉型計畫中，除了將大幅擁抱電動車發展，更預計在2030年以前於全球市場實現40%銷售車款為電動車，在2025年後更使電動車產品營收可達300億美元規模，其中也包含運用既有資源創造全新車輛價值。 近期除了攜手線上支付業者Stripe，藉此跨入線上電商市場競爭，福特更傳出將加速其電動車市場佈局腳步，預計在電動車產品發展增加投資200億美元，藉此加速電動車產品普及化。 彭博新聞引述消息來源指出，此項由前蘋果及Tesla主管Doug Field推動改革，將使福特在未來5-10年間額外增加100億至20…",
+    "url": "https://www.cool3c.com/article/172502",
+    "urlToImage": "https://sw.cool3c.com/article/2022/94c9bf1b-b6ca-48cb-bbf2-0103febebb06.jpg",
+    "publishedAt": "2022-02-06T11:13:23Z",
+    "content": "Ford+203040%2025300\r\nStripe200\r\nTeslaDoug Field5-10100200\r\n2025300Ford+\r\nFord+\r\nFord+203040%2025300"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Frandroid"
+    },
+    "author": "Melinda Davan-Soulas",
+    "title": "PlayStation rallie Bungie à sa cause, concurrence sérieuse pour Tesla, Lidl à vélo – L’essentiel de l’actu de la semaine",
+    "description": "Après Microsoft, au tour de Sony de sortir (plus timidement) le chéquier pour s'offrir Bungie (Halo, Destiny). Les Samsung Galaxy S22 arrivent et on sait déjà quasiment tout sur eux et Lidl se met au vélo. Retour sur l'actu tech de la semaine.Sony rachète Bun…",
+    "url": "https://www.frandroid.com/actualites-generales/1217989_playstation-rallie-bungie-a-sa-cause-concurrence-serieuse-pour-tesla-lidl-a-velo-lessentiel-de-lactu-de-la-semaine",
+    "urlToImage": "https://images.frandroid.com/wp-content/uploads/2021/04/ps5-scaled.jpg",
+    "publishedAt": "2022-02-06T11:00:43Z",
+    "content": "Après Microsoft, au tour de Sony de sortir (plus timidement) le chéquier pour s'offrir Bungie (Halo, Destiny). Les Samsung Galaxy S22 arrivent et on sait déjà quasiment tout sur eux et Lidl se met au… [+2263 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Teltarif.de"
+    },
+    "author": "Alexander Kuch",
+    "title": "Nur nett oder auch nützlich: Was bringen Auto-Apps?",
+    "description": "Die meisten Autohersteller bieten inzwischen Apps an, mit denen sich Funktionen am und rund ums Auto steuern lassen. Ist das eine sinnvolle Ergänzung zum Fahrzeug oder am Ende doch nur Spielerei?",
+    "url": "https://www.teltarif.de/auto-apps-empfehlung-auto-motor-sport/news/87142.html",
+    "urlToImage": "https://www.teltarif.de/img/arch/2022/kw05/auto-apps-empfehlung-auto-motor-sport-1og.jpg",
+    "publishedAt": "2022-02-06T10:56:01Z",
+    "content": "Ein iPhone als Autoschlüssel, hier bei BMWBild: BMW\r\nMit ein paar Finger­tipps steht die\r\nRoute. Geplant wird abends vom Sofa aus. Ein weiterer Tipp beför­dert\r\ndie Route dann ins Navi im Auto. Am nä… [+8241 chars]"
+    },
+    {
+    "source": {
+    "id": "wirtschafts-woche",
+    "name": "Wirtschafts Woche"
+    },
+    "author": "Sören Imöhl",
+    "title": "Bitcoin-Kurs aktuell: Bitcoin weiter deutlich über 40.000-Dollar-Marke",
+    "description": "Nach dem Tief im Mai waren viele Kryptowährungen wieder im Aufwind, so auch die älteste von ihnen. Der Bitcoin fiel aber am Montag auf den tiefsten Stand seit September. Nun beginnt scheinbar die Erholung.",
+    "url": "https://www.wiwo.de/finanzen/boerse/bitcoin-kurs-aktuell-bitcoin-weiter-deutlich-ueber-40-000-dollar-marke/27382428.html",
+    "urlToImage": "https://www.wiwo.de/images/bitcoin-kurs-aktuell/27382784/2-format11240.jpg",
+    "publishedAt": "2022-02-06T10:47:57Z",
+    "content": "Aktuelle News und Nachrichten von heute zum Bitcoin und zum aktuellen Kurs\r\n6. Februar 2022 - Bitcoin hält Kurserholung\r\nDie Kursgewinne der Vortage konnte der Bitcoin auch am Sonntag zunächst halten… [+36654 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Hotnews.ro"
+    },
+    "author": "https://www.facebook.com/www.hotnews.ro",
+    "title": "Noua problemă de greutate a Americii: Mașinile electrice",
+    "description": "Deși \"revoluția\" electrică pare inevitabilă (atât datorită beneficiilor cât și penalităților impuse tot mai des vehiculelor convenționale), mașinile electrice se confruntă cu suficiente provocări: autonomia, costurile, disponibilitatea sau deprecierea. Însă m…",
+    "url": "https://economie.hotnews.ro/stiri-auto-25349860-noua-problema-greutate-americii-masinile-electrice.htm",
+    "urlToImage": "http://media.hotnews.ro/media_server1/image-2020-11-2-24391359-70-masini-electrice-incarcat.jpg",
+    "publishedAt": "2022-02-06T10:25:00Z",
+    "content": "Dei „revoluia” electric pare inevitabil (atât datorit beneficiilor cât i penalitilor impuse tot mai des vehiculelor convenionale), mainile electrice se confrunt cu suficiente provocri: autonomia, cos… [+4470 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Digi24.ro"
+    },
+    "author": "Digi24",
+    "title": "Tânărul aflat în spatele contului de Twitter care monitorizează avionul privat al lui Elon Musk refuză o nouă ofertă",
+    "description": "Jack Sweeney, un tânăr de 19 ani din Florida aflat în spatele unui cont de Twitter care monitorizează avionul privat al lui Elon Musk, a refuzat o nouă ofertă pentru a șterge contul, relatează Business Insider.",
+    "url": "https://www.digi24.ro/stiri/externe/tanarul-aflat-in-spatele-contului-de-twitter-care-monitorizeaza-avionul-privat-al-lui-elon-musk-refuza-o-noua-oferta-1828723",
+    "urlToImage": "https://s.iw.ro/gateway/g/ZmlsZVNvdXJjZT1odHRwJTNBJTJGJTJG/c3RvcmFnZTA3dHJhbnNjb2Rlci5yY3Mt/cmRzLnJvJTJGc3RvcmFnZSUyRjIwMjEl/MkYxMCUyRjI2JTJGMTQwNTQzOV8xNDA1/NDM5X2Vsb24tbXVzay5qcGcmdz0xMjAw/Jmg9NjMwJnpjPTEmaGFzaD1mZjUwNDRm/ZjFmNWY0ODA5ZjMzNzVjZjNhOThhYWRkMA==.thumb.jpg",
+    "publishedAt": "2022-02-06T10:06:29Z",
+    "content": "Jack Sweeney, un tânr de 19 ani din Florida aflat în spatele unui cont de Twitter care monitorizeaz avionul privat al lui Elon Musk, a refuzat o nou ofert pentru a terge contul, relateaz Business Ins… [+1197 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Wall-street.ro"
+    },
+    "author": "office@wall-street.ro (Redactia Wall-Street)",
+    "title": "Tânărul care spionează avionul privat al lui Elon Musk a refuzat Tesla oferită de miliardar",
+    "description": "Tânărul de 19 ani care spionează avionul privat al lui Elon Musk a refuzat oferta acestuia de a primi o Tesla Model 3 în schimbul ștergerii mesajului de pe Twitter, care arată locația avionului.",
+    "url": "https://www.wall-street.ro/articol/International/281667/tanarul-care-spioneaza-avionul-privat-al-lui-elon-musk-a-refuzat-tesla-oferita-de-miliardar.html",
+    "urlToImage": "https://www.wall-street.ro/article_pictures/281667_articol.jpg?v=1644145318",
+    "publishedAt": "2022-02-06T10:06:23Z",
+    "content": "Tânrul de 19 ani care spioneaz avionul privat al lui Elon Musk a refuzat oferta acestuia de a primi o Tesla Model 3 în schimbul tergerii mesajului de pe Twitter, care arat locaia avionului.\r\nDin vara… [+2669 chars]"
+    },
+    {
+    "source": {
+    "id": "wirtschafts-woche",
+    "name": "Wirtschafts Woche"
+    },
+    "author": "Jannik Deters",
+    "title": "Giga Factory in Grünheide: „Erstaunlich, wie schnell das geht – wo doch alle nur von Lieferengpässen und Corona reden“",
+    "description": "Der Produktionsstart der Tesla-Fabrik in Grünheide verzögert sich weiter. Der dortige Bürgermeister Arne Christiani erklärt, warum ihn weder das noch die anstehende Gerichtsverhandlung wegen Wasserlieferungen an den US-Autobauer beunruhigen.",
+    "url": "https://www.wiwo.de/unternehmen/auto/giga-factory-in-gruenheide-erstaunlich-wie-schnell-das-geht-wo-doch-alle-nur-von-lieferengpaessen-und-corona-reden/28039766.html",
+    "urlToImage": "https://www.wiwo.de/images/baustelle-tesla-gigafactory-berlin-brandenburg/28040102/2-format11240.jpg",
+    "publishedAt": "2022-02-06T10:03:56Z",
+    "content": "Wann fällt der Startschuss für die Tesla-Produktion in Grünheide (Mark), Brandenburg? Noch hat das zuständige Landesamt in Potsdam die endgültige Genehmigung nicht erteilt, momentan deutet vieles auf… [+8070 chars]"
+    },
+    {
+    "source": {
+    "id": "la-repubblica",
+    "name": "La Repubblica"
+    },
+    "author": "repubblicawww@repubblica.it (Redazione Repubblica.it)",
+    "title": "Torna la Kitt di Supercar. Stavolta è una Tesla",
+    "description": "Il modello realizzato da uno youtubber americano. Ecco il risultato a cominciare dall'incredibile volante",
+    "url": "https://www.repubblica.it/motori/sezioni/attualita/2022/02/06/news/torna_la_kitt_di_supercar_stavolta_e_una_tesla-336682319/",
+    "urlToImage": "https://www.repstatic.it/content/nazionale/img/2022/02/06/110058156-87fa09dc-5701-4405-bc55-9b4b8058db43.jpg",
+    "publishedAt": "2022-02-06T10:02:15Z",
+    "content": "ROMA - Tra le serie tv indimenticabili c'è senza dubbio Supercar: è lì che si trova la mamma di tutte le auto a guida autonoma, quando ancora si pensava che una macchina senza pilota potesse esistere… [+1917 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Seeking Alpha"
+    },
+    "author": "John M. Mason",
+    "title": "The Stock Market: Volatility Rules",
+    "description": "Uncertainty is ruling the stock market these days. Investors seem seem to be responding to the uncertainty by moving money here and there. Volatility will continue.",
+    "url": "https://seekingalpha.com/article/4484701-stock-market-volatility-rules",
+    "urlToImage": "https://static.seekingalpha.com/cdn/s3/uploads/getty_images/1149947897/image_1149947897.jpg?io=getty-c-w750",
+    "publishedAt": "2022-02-06T09:49:53Z",
+    "content": "DNY59/iStock via Getty Images\r\nVolatility is dominating the stock market these days.\r\n In other words, uncertainty seems to be everywhere.\r\n The index of market volatility, the VIX index has spent th… [+5941 chars]"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Yahoo Entertainment"
+    },
+    "author": "蕭婷方",
+    "title": "高雄「引水人」爆染疫接觸40艘船，打三劑仍確診，足跡曝光！",
+    "description": "今天春節連假最後一天，年節期間新冠疫情未曾停歇，每天皆有新增本土個案。中央流行疫情指揮中心公布，國內今新增43例新冠肺炎（COVID-19）確定病例，分別為14例本土個案、29例境外移入。其中，高雄港區今天傳出有「引水人」確診，先前沒有被居家隔離，也非高雄港確診個案的接觸者，感染源不明；此外，感染期間多次通傳篩檢也未到場，更登上逾40艘船作業，足跡遍佈高雄港熱門景點引發關注。",
+    "url": "https://tw.news.yahoo.com/%E9%AB%98%E9%9B%84-%E5%BC%95%E6%B0%B4%E4%BA%BA-%E7%88%86%E6%9F%93%E7%96%AB%E6%8E%A5%E8%A7%B840%E8%89%98%E8%88%B9-%E6%89%93%E4%B8%89%E5%8A%91%E4%BB%8D%E7%A2%BA%E8%A8%BA-%E8%B6%B3%E8%B7%A1%E6%9B%9D%E5%85%89-093503057.html",
+    "urlToImage": "https://s.yimg.com/uu/api/res/1.2/j9AlDRwdkmp2hucG1a7bIg--~B/aD00Mzk7dz01OTI7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/ko/businesstoday.com.tw/bff86733bc951b7ba8a8cfef8592a9c6",
+    "publishedAt": "2022-02-06T09:35:03Z",
+    "content": "6042022266057367002001853%202211768817.8431%(ADR)7294.6(NVIDIA)(Berkshire Hathaway)8(Apple)(Microsoft)(Saudi Aramco)Alphabet(Amazon)(Tesla)Meta 5271994955"
+    },
+    {
+    "source": {
+    "id": null,
+    "name": "Euronews"
+    },
+    "author": null,
+    "title": "Tech this week: El Salvador rejects IMF call to drop Bitcoin use - Euronews",
+    "description": "El Salvador became the first country in the world to make Bitcoin legal tender and is resisting any pushback on that, while Snap shares surge after a tough week for the tech sector.",
+    "url": "https://www.euronews.com/next/2022/02/06/tech-this-week-el-salvador-rejects-imf-call-to-drop-bitcoin-use-and-snap-shares-surge-50",
+    "urlToImage": "https://static.euronews.com/articles/stories/06/45/13/02/1000x563_cmsv2_979248af-6156-5165-b594-e4a459df1f7e-6451302.jpg",
+    "publishedAt": "2022-02-06T09:30:29Z",
+    "content": "The government of El Salvador on Monday rejected a recommendation by the International Monetary Fund (IMF) to drop Bitcoin as legal tender in the Central American country.\r\nTreasury minister Alejandr… [+7138 chars]"
+    }
+    ]
+    }
